c625dc677a34a59e6e12929c1cfe37dd82301a4b
andreus-dot-focus <vorkozhokovandrey@gmail.com>
2021-03-05 17:20:32 +0700

    calculatuons class

---
+++ SIMS_3/Calculations.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_3
+{
+    class Calculations
+    {
+        const decimal g = 9.81M;
+        const decimal C = 0.15M;
+        const decimal rho = 1.29M;
+
+        const decimal dt = 0.1M;
+
+        decimal cosa;
+        decimal sina;
+
+        decimal k;
+        decimal vx;
+        decimal vy;
+
+        double a;
+
+        decimal x;
+        decimal y;
+        decimal t;
+
+        public Calculations(double a, decimal v0, decimal y0, decimal S, decimal m){
+            this.a = a;
+
+            cosa = (decimal)Math.Cos(a);
+            sina = (decimal)Math.Sin(a);
+
+            k = 0.5M * C * rho * S / m;
+            vx = v0 * cosa;
+            vy = v0 * sina;
+
+            t = 0;
+            x = 0;
+            y = y0;
+        }
+
+        public void RungeKutta() {
+            t += dt * 10000;
+
+            decimal v = (decimal)Math.Sqrt((double)(vx * vx + vy * vy));
+            vx = vx - k * vx * v * dt;
+            vy = vy - (g + k * vy * v) * dt;
+            x = x + vx * dt;
+            y = y + vy * dt;
+        }
+
+        public decimal GetX() 
+        {
+            return x;
+        }
+
+        public decimal GetY()
+        {
+            return y;
+        }
+
+        public decimal GetTime() {
+            return t;
+        }
+    }
+}
--- SIMS_3/Form1.cs
+++ SIMS_3/Form1.cs
@@ -12,29 +12,8 @@ namespace SIMS_3
 {
     public partial class Form1 : Form
     {
-        const decimal g = 9.81M;
-        const decimal C = 0.15M;
-        const decimal rho = 1.29M;
-
-        decimal cosa;
-        decimal sina;
-        decimal S;
-        decimal m;
-        decimal k;
-        decimal vx;
-        decimal vy;
-
-        decimal dt;
-
-        double a;
-        decimal v0;
-        decimal y0;
-
-        decimal t;
-        decimal x;
-        decimal y;
-
         bool isModeling = false;
+        Calculations calc;
 
         public Form1()
         {
@@ -45,25 +24,10 @@ namespace SIMS_3
         {
             if (isModeling == false)
             {
-                dt = 0.1M;
-                a = (double)edAngle.Value * Math.PI / 180;
-                v0 = (decimal)edSpeed.Value;
-                y0 = (decimal)edHeight.Value;
+                calc = new Calculations((double)edAngle.Value * Math.PI / 180, edSpeed.Value, edHeight.Value, edSize.Value, edWeight.Value);
 
-                cosa = (decimal)Math.Cos(a);
-                sina = (decimal)Math.Sin(a);
-                S = edSize.Value;
-                m = edWeight.Value;
-                k = 0.5M * C * rho * S / m;
-                vx = v0 * cosa;
-                vy = v0 * sina;
-
-
-                t = 0;
-                x = 0;
-                y = y0;
                 chart1.Series[0].Points.Clear();
-                chart1.Series[0].Points.AddXY(x, y);
+                chart1.Series[0].Points.AddXY(calc.GetX(), calc.GetY());
 
                 isModeling = true;
             }
@@ -72,17 +36,12 @@ namespace SIMS_3
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            t += dt*10000;
+            decimal t = calc.GetTime();
             timeValueLabel.Text = $"{(int)t / 60000}.{(int)t / 1000}.{(int)t % 1000}";
 
-            decimal v = (decimal)Math.Sqrt((double)(vx * vx + vy * vy));
-            vx = vx - k * vx * v * dt;
-            vy = vy - (g + k * vy * v) * dt;
-            x = x + vx * dt;
-            y = y + vy * dt;
-
-            chart1.Series[0].Points.AddXY(x, y);
-            if (y <= 0)
+            calc.RungeKutta();
+            chart1.Series[0].Points.AddXY(calc.GetX(), calc.GetY());
+            if (calc.GetY() <= 0)
             {
                 timer1.Stop();
                 isModeling = false;
